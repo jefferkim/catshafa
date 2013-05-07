c45203cc0a7f320954df90faf087bf9be77aaa26
jinjianfeng <wb-jinjianfeng@taobao.com>
2013-05-07 14:11:41 +0800

    edit the sidebar style

--- searchform.php
+++ searchform.php
@@ -14,7 +14,7 @@
 	
 	<div>
 		
-		<input class="search-text" type="text" name="s" value="<?php if ( is_search() ) echo esc_attr( get_search_query() ); else esc_attr_e( '搜索本站', 'origin' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
+		<input class="search-text" type="text" name="s" value="<?php if ( is_search() ) echo esc_attr( get_search_query() ); else esc_attr_e( '', 'origin' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
 		
 		<input class="search-submit button" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'origin' ); ?>" />
 		
